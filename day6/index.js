61711dbe3696bb9f6223233e738d7baaa73a5c79
Bassel-11 <albassel.abobakr11@gmail.com>
2025-09-22 20:51:24 +0200

    connection problem

--- day6/index.js
+++ day6/index.js
@@ -1,2 +1,23 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://albasselabobakr:<db_password>@learn-mongo-db.pdwl7rh.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db";
+
+const url = "mongodb+srv://albasselabobakr:albassel@learn-mongo-db.pdwl7rh.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db";
+const client = new MongoClient(url);
+
+const main = async () => {
+  try {
+    await client.connect();
+    console.log("✅ Connected successfully to db");
+
+    const db = client.db("codeZone");
+    const collection = db.collection("courses");
+
+    const data = await collection.find().toArray();
+    console.log("data:", data);
+  } catch (err) {
+    console.error("❌ Error:", err);
+  } finally {
+    await client.close();
+  }
+};
+
+main();
