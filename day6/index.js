e75dde50d6eb254b55bb2435e19703fd6906a472
Bassel-11 <albassel.abobakr11@gmail.com>
2025-09-22 14:53:53 +0300

    mongo

---
+++ day6/index.js
@@ -0,0 +1,2 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://albasselabobakr:<db_password>@learn-mongo-db.pdwl7rh.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db";
